09cbf45e2ac2b57bc1d70614209fb72216da8526
mayamahroum <58133999+mayamahroum@users.noreply.github.com>
2026-02-24 10:28:54 +0100

    remove version 2 completely

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Version2 from './Version2.jsx'
 
 if (import.meta.env.PROD && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -10,5 +9,5 @@ if (import.meta.env.PROD && 'serviceWorker' in navigator) {
 }
 
 createRoot(document.getElementById('root')).render(
-  window.location.pathname.startsWith('/version2') ? <Version2 /> : <App />,
+  <App />,
 )
